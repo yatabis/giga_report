bc39e4ce52eb704ccc13a5f5f2cb34d2e1bcb56e
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-01-27 20:59:17 +0900

    fix: setup for chromedriver

--- main.py
+++ main.py
@@ -24,21 +24,23 @@ def fetch_giga():
     # options.add_argument('--headless')
     # options.add_argument('--disable-gpu')
     # driver = webdriver.Chrome(chrome_options=options)
-    driver = webdriver.Chrome()
+    options= webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
 
     driver.get(os.environ.get('LOGIN_URL'))
     driver.find_element_by_name('telnum').send_keys(os.environ.get('TEL_NUM'))
     driver.find_element_by_name('password').send_keys(os.environ.get('PASSWORD'))
     driver.find_element_by_xpath("//input[@value='ログインする']").click()
-    print([cookie['name'] for cookie in driver.get_cookies()])
+    # print([cookie['name'] for cookie in driver.get_cookies()])
     driver.find_element_by_xpath("//*[@id='use-data']/div/div/div[1]/p/img").click()
     gb = driver.find_element_by_xpath(
-        "//*[@id='contents-body']/form/div[3]/div/div/div[2]/table/tbody/tr[2]/th/div/div[2]/span")
+        "//*[@id='contents-body']/form/div[3]/div/div/div[2]/table/tbody/tr[2]/th/div/div[2]/span").text
 
     driver.close()
     driver.quit()
 
-    return gb.text
+    return gb
 
 
 @route('/callback', method='POST')
@@ -60,4 +62,6 @@ def callback():
 
 
 if __name__ == '__main__':
+    print(fetch_giga())
+else:
     run(host='0.0.0.0', port=int(os.environ.get('PORT', 443)))
