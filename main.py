ce163c7659aa7df10eaa3ccfa54b22bb48d3bdb5
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-01-27 21:16:00 +0900

    fix: setup for chromedriver

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from pprint import pprint
 from bottle import run, route, request
 import requests
 from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
 import chromedriver_binary
 
 CAT = os.environ.get('CHANNEL_ACCESS_TOKEN')
@@ -15,27 +14,25 @@ HEADER = {'Content-Type': 'application/json', 'Authorization': f"Bearer {CAT}"}
 def reply_text(text, token):
     ep = "https://api.line.me/v2/bot/message/reply"
     body = {'replyToken': token, 'messages': [{'type': 'text', 'text': text}]}
-    return requests.post(ep, data=json.dumps(body, ensure_ascii=False).encode('utf-8'), headers=HEADER)
+    requests.post(ep, data=json.dumps(body, ensure_ascii=False).encode('utf-8'), headers=HEADER)
 
 
 def fetch_giga():
-    # options = Options()
-    # options.binary_location = '/app/.apt/usr/bin/google-chrome'
-    # options.add_argument('--headless')
-    # options.add_argument('--disable-gpu')
-    # driver = webdriver.Chrome(chrome_options=options)
     options= webdriver.ChromeOptions()
     options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
 
     driver.get(os.environ.get('LOGIN_URL'))
+    print(driver.current_url)
     driver.find_element_by_name('telnum').send_keys(os.environ.get('TEL_NUM'))
     driver.find_element_by_name('password').send_keys(os.environ.get('PASSWORD'))
     driver.find_element_by_xpath("//input[@value='ログインする']").click()
-    # print([cookie['name'] for cookie in driver.get_cookies()])
+    print(driver.current_url)
     driver.find_element_by_xpath("//*[@id='use-data']/div/div/div[1]/p/img").click()
+    print(driver.current_url)
     gb = driver.find_element_by_xpath(
         "//*[@id='contents-body']/form/div[3]/div/div/div[2]/table/tbody/tr[2]/th/div/div[2]/span").text
+    print(gb)
 
     driver.close()
     driver.quit()
@@ -57,8 +54,7 @@ def callback():
             else:
                 reply_text("今月のデータ残量が知りたいんだね？\nわかった、調べてくるよ！\nちょっと待っててね！", reply_token)
                 giga = fetch_giga()
-                result = reply_text(f"おまたせ！\n今月のデータ残量は {giga} GBだよ!", reply_token)
-                pprint(result)
+                reply_text(f"おまたせ！\n今月のデータ残量は {giga} GBだよ!", reply_token)
 
 
 if __name__ == '__main__':
