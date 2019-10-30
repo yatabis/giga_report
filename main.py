ec3b3a1d3bbac1202b79d9b1109822211f04ed13
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-10-30 17:42:53 +0900

    add: エラーハンドリング

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import chromedriver_binary
 import psycopg2
 import requests
 from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 
 CAT = os.environ.get('CHANNEL_ACCESS_TOKEN')
 MASTER = os.environ.get('MASTER')
@@ -37,28 +38,37 @@ def get_connection():
 
 def fetch_giga():
     options = webdriver.ChromeOptions()
-    options.binary_location = os.environ.get("GOOGLE_CHROME_SHIM")
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(options=options)
 
-    driver.get(os.environ.get('LOGIN_URL'))
-    print(driver.current_url)
-    driver.find_element_by_name('telnum').send_keys(os.environ.get('TEL_NUM'))
-    driver.find_element_by_name('password').send_keys(os.environ.get('PASSWORD'))
-    driver.find_element_by_xpath("//input[@value='ログインする']").click()
-    print(driver.current_url)
-    driver.find_element_by_xpath("//*[@id='use-data']/div/div/div[1]/p/img").click()
-    print(driver.current_url)
-    driver.implicitly_wait(5)
-    gb = driver.find_element_by_css_selector("span.remain.nums.fs-24").text
-    print(driver.current_url)
-    print(gb)
-    driver.find_element_by_id('js-toggle-menu').click()
-    driver.find_element_by_xpath('//*[@id="js-toggle-menu-contents"]/p[2]/a').click()
-    time.sleep(2)
-    print(driver.current_url)
-
+    try:
+        driver.get(os.environ.get('LOGIN_URL'))
+        print(driver.current_url)
+        driver.find_element_by_name('telnum').send_keys(os.environ.get('TEL_NUM'))
+        driver.find_element_by_name('password').send_keys(os.environ.get('PASSWORD'))
+        driver.find_element_by_xpath("//input[@value='ログインする']").click()
+        print(driver.current_url)
+        driver.find_element_by_xpath("//*[@id='use-data']/div/div/div[1]/p/img").click()
+        print(driver.current_url)
+        driver.implicitly_wait(5)
+        gb = driver.find_element_by_css_selector("span.remain.nums.fs-24").text
+        print(driver.current_url)
+        print(gb)
+        driver.find_element_by_id('js-toggle-menu').click()
+        driver.find_element_by_xpath('//*[@id="js-toggle-menu-contents"]/p[2]/a').click()
+        time.sleep(2)
+        print(driver.current_url)
+    except ElementClickInterceptedException:
+        print("クリック関連のエラーが発生しました。")
+        gb = "err"
+    except NoSuchElementException:
+        print("要素が存在しないエラーが発生しました。")
+        gb = "err"
+    except Exception:
+        print("予期していなかったエラーが発生しました。")
+        gb = "err"
+        
     driver.close()
     driver.quit()
 
@@ -140,6 +150,8 @@ def timed_report():
     debug = os.environ.get('DEBUG', False)
 
     giga = fetch_giga()
+    if giga == "err":
+        return
     latest = float(fetch_db('latest'))
     interval = int(fetch_db('interval'))
     # デバッグ用
