e44c02e85c0f81d18c5d15da8012d04c38e16c67
yatabis <39yatabis.rim8820xxx@gmail.com>
2019-10-30 18:11:22 +0900

    fix: クリック関連のバグ修正

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import chromedriver_binary
 import psycopg2
 import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 
 CAT = os.environ.get('CHANNEL_ACCESS_TOKEN')
@@ -41,6 +44,7 @@ def fetch_giga():
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(options=options)
+    wait = WebDriverWait(driver, 2)
 
     try:
         driver.get(os.environ.get('LOGIN_URL'))
@@ -55,22 +59,29 @@ def fetch_giga():
         gb = driver.find_element_by_css_selector("span.remain.nums.fs-24").text
         print(driver.current_url)
         print(gb)
+        driver.execute_script("window.scrollTo({top: 1000, behavior: 'instant'});")
         driver.find_element_by_id('js-toggle-menu').click()
-        driver.find_element_by_xpath('//*[@id="js-toggle-menu-contents"]/p[2]/a').click()
+        logout = wait.until(expected_conditions.element_to_be_clickable((By.XPATH, '//*[@id="js-toggle-menu-contents"]/p[2]/a')))
+        logout.click()
         time.sleep(2)
         print(driver.current_url)
+        driver.close()
+        driver.quit()
     except ElementClickInterceptedException:
         print("クリック関連のエラーが発生しました。")
-        gb = "err"
+        driver.close()
+        driver.quit()
+        return "err"
     except NoSuchElementException:
         print("要素が存在しないエラーが発生しました。")
-        gb = "err"
+        driver.close()
+        driver.quit()
+        return "err"
     except Exception:
         print("予期していなかったエラーが発生しました。")
-        gb = "err"
-        
-    driver.close()
-    driver.quit()
+        driver.close()
+        driver.quit()
+        return "err"
 
     return float(gb)
 
